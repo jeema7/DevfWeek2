34e5f4d34d00eff2fa00339fb8f7db17c5847130
jeema7 <89801113+jeema7@users.noreply.github.com>
2022-08-19 02:06:54 -0500

    create book

--- api/controllers/bookController.js
+++ api/controllers/bookController.js
@@ -1,9 +1,27 @@
+import Book from '../model/Book.js'
+
+
 const getAllBooks = (req,res) =>{
     return res.json({
         msg: 'Esta es la ruta GET de todos los libros'
     });
 }
-const createBook = (req,res) =>{}
+const createBook = async(req,res) =>{
+    try {
+        const newBook = await Book.create(req.body)
+        return res.json({
+          msg: 'Libro creado',
+          book: newBook,
+        })
+        
+    } catch (error) {   
+        return res.json({
+            msg: 'Error al crear un libro',
+        });
+        
+    }
+
+};
 const getBookById = (req,res) =>{}
 const updateBookById = (req,res) =>{}
 const deleteBookByid = (req,res) =>{}
